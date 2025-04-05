95057b3d846ce6024e93e3bb92bedf1b9a5006ce
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2025-04-05 15:08:07 +0200

    add smooth scrolling for nav links

--- script.js
+++ script.js
@@ -29,4 +29,15 @@ document.addEventListener("DOMContentLoaded", function () {
       document.body.classList.remove("no-scroll");
     });
   });
+
+  //  smooth scrolling to all nav links
+  navItems.forEach(item => {
+    item.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href");
+      document.querySelector(targetId).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
 });
